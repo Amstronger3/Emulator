1e694caaba63bc6f5ca5593df4127d7e9dff7803
aleksandr <amstronger3@gmai.com>
2021-08-30 14:36:22 +0300

    update docker

--- emulator_test/emulator_test/settings.py
+++ emulator_test/emulator_test/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'emulator',
         'USER': 'emulatoruser',
         'PASSWORD': 'emulator',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
@@ -131,4 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CELERY_TIMEZONE = "Europe/Kiev"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+# CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = "redis://redis:6379"
+CELERY_RESULT_BACKEND = "redis://redis:6379"
