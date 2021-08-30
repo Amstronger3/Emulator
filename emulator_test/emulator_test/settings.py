6095addbb36d7a58d71f32af20525aa9a7531cfb
aleksandr <amstronger3@gmai.com>
2021-08-30 21:39:47 +0300

    update

--- emulator_test/emulator_test/settings.py
+++ emulator_test/emulator_test/settings.py
@@ -132,8 +132,5 @@ CELERY_TIMEZONE = "Europe/Kiev"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 # CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_BROKER_URL = "redis://redis:6379"
-CELERY_RESULT_BACKEND = "redis://redis:6379"
-
-
-# sheltered-dawn-19406
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
